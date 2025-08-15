75f615cbfc8d86480d092774f6cd5a1aa9de59e2
Mihai Dragusin <33466422+MNDragusin@users.noreply.github.com>
2025-08-15 12:54:12 +0300

    undo functionality for removing an entry & handling empty list

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -15,31 +15,57 @@ class Expenses extends StatefulWidget {
 }
 
 class _Expenses extends State<Expenses> {
-
   final List<Expense> _registeredExpenses = mockExpenses;
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       isScrollControlled: true,
       context: context,
-      builder: (ctx) => NewExpense(_addExpense)
+      builder: (ctx) => NewExpense(_addExpense),
     );
   }
-  
+
   void _addExpense(Expense expense) {
     setState(() {
       _registeredExpenses.add(expense);
     });
   }
 
-  void _removeExpense(Expense expense){
+  void _removeExpense(Expense expense) {
+    final index = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () => {
+            setState(() {
+              _registeredExpenses.insert(index, expense);
+            }),
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some.'),
+    );
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Wallet'),
@@ -51,10 +77,7 @@ class _Expenses extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Expanded(child: ExpensesList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense,))],
-      ),
+      body: Column(children: [Expanded(child: mainContent)]),
     );
   }
 }
