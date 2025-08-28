78aef0c5984f16513f8f13149564c6f21e707a9a
Mihai Dragusin <33466422+MNDragusin@users.noreply.github.com>
2025-08-28 18:36:25 +0300

    moved theme data to a separate file

---
+++ lib/app_theme.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+
+ThemeData getDarkTheme() {
+  return ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        // appBarTheme: const AppBarTheme().copyWith(
+        //   backgroundColor: kDarkColorScheme.onPrimaryContainer,
+        //   foregroundColor: kDarkColorScheme.primaryContainer,
+        // ),
+        cardTheme: const CardThemeData().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kDarkColorScheme.onSecondaryContainer,
+            fontSize: 24,
+          ),
+        ),
+      );
+}
+
+ThemeData getLightTheme(){
+return ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 24,
+          ),
+        ),
+      );
+}
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 58, 181),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
